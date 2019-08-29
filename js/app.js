dc40e46881acb2ff7a15bcb8496a66d1a5dfae6e
Bangula <bvukovic082@gmail.com>
2019-08-29 10:57:49 +0200

    project setup

---
+++ js/app.js
@@ -0,0 +1,10 @@
+fetch("https://api.myjson.com/bins/1gwnal")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+
+    let time = data[0].timestamp;
+    let date = new Date(time);
+    let newDate = date.toString(); // Wed Jan 12 2011 12:42:46 GMT-0800 (PST)
+  })
+  .catch(err => console.log(err));
